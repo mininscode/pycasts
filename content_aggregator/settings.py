102f39c900d671224fff57f845820e0a228051ed
mininscode <mininscode@gmail.com>
2021-11-30 14:58:03 +0300

    Make settings for managing credentials

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -11,19 +11,19 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dynaconf import settings as config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!5g+o!(%q)@jejzp3r9s(vyg$l00t54r5o8&1x)mc&ql&!0kr('
-
+SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.DEBUG
 
 ALLOWED_HOSTS = []
 
