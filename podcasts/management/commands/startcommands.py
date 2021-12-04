104c80b3d007082f014398733c25eecb5cfe00c0
mininscode <mininscode@gmail.com>
2021-12-04 19:10:54 +0300

    Add django start commands for podacsts app

---
+++ podcasts/management/commands/startcommands.py
@@ -0,0 +1,29 @@
+import ssl
+import feedparser
+from dateutil import parser
+from django.core.management.base import BaseCommand
+
+from podcasts.models import Episode
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if hasattr(ssl, '_create_unverified_context'):
+            ssl._create_default_https_context = ssl._create_unverified_context
+        feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+        podcast_title = feed.channel.title
+        podcast_image = feed.channel.image['href']
+
+        for item in feed.entries:
+            if not Episode.objects.filter(guid=item.guid).exists():
+                episode = Episode(
+                    title=item.title,
+                    description=item.description,
+                    pub_date=parser.parse(item.published),
+                    link=item.link,
+                    image=podcast_image,
+                    podcast_name=podcast_title,
+                    guid=item.guid,
+                )
+                episode.save()
+
